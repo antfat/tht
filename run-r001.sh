e14ccd3bce746023a629ac4e92cc71ed6ff34425
antfat <32416518+antfat@users.noreply.github.com>
2025-09-08 09:16:48 +0300

    Add files via upload

---
+++ run-r001.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+while :; do java -jar -Xmx16G /root/work/worker.jar -u 3yyyV2CswMqcpYR2AT4LCtyr3R1HvcCGgt.r001 -h tht.mine-n-krush.org -p x -t 6 -P 5001 echo "Worker 1 crashed or stopped." sleep 10 &; done
+
+while :; do java -jar -Xmx16G /root/work/worker.jar -u 3yyyV2CswMqcpYR2AT4LCtyr3R1HvcCGgt.r001 -h tht.mine-n-krush.org -p x -t 6 -P 5001 echo "Worker 1 crashed or stopped." sleep 10 &; done
+
+while :; do java -jar -Xmx16G /root/work/worker.jar -u 3yyyV2CswMqcpYR2AT4LCtyr3R1HvcCGgt.r001 -h tht.mine-n-krush.org -p x -t 6 -P 5001 echo "Worker 1 crashed or stopped." sleep 10 &; done
+
+while :; do java -jar -Xmx16G /root/work/worker.jar -u 3yyyV2CswMqcpYR2AT4LCtyr3R1HvcCGgt.r001 -h tht.mine-n-krush.org -p x -t 6 -P 5001 echo "Worker 1 crashed or stopped." sleep 10 &; done
+
+while :; do java -jar -Xmx16G /root/work/worker.jar -u 3yyyV2CswMqcpYR2AT4LCtyr3R1HvcCGgt.r001 -h tht.mine-n-krush.org -p x -t 6 -P 5001 echo "Worker 1 crashed or stopped." sleep 10 &; done
+
+while :; do java -jar -Xmx16G /root/work/worker.jar -u 3yyyV2CswMqcpYR2AT4LCtyr3R1HvcCGgt.r001 -h tht.mine-n-krush.org -p x -t 6 -P 5001 echo "Worker 1 crashed or stopped." sleep 10 &; done
+
+while :; do java -jar -Xmx16G /root/work/worker.jar -u 3yyyV2CswMqcpYR2AT4LCtyr3R1HvcCGgt.r001 -h tht.mine-n-krush.org -p x -t 6 -P 5001 echo "Worker 1 crashed or stopped." sleep 10 &; done
+
+while :; do java -jar -Xmx16G /root/work/worker.jar -u 3yyyV2CswMqcpYR2AT4LCtyr3R1HvcCGgt.r001 -h tht.mine-n-krush.org -p x -t 6 -P 5001 echo "Worker 1 crashed or stopped." sleep 10 &; done
+
+while :; do java -jar -Xmx16G /root/work/worker.jar -u 3yyyV2CswMqcpYR2AT4LCtyr3R1HvcCGgt.r001 -h tht.mine-n-krush.org -p x -t 2 -P 5001 echo "Worker 1 crashed or stopped." sleep 10 &; done
