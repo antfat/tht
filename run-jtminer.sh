0fbc3381ac479ea926515df126788e40da228ce3
antfat <32416518+antfat@users.noreply.github.com>
2025-09-08 07:19:35 +0300

    Add files via upload

---
+++ run-jtminer.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+java -jar -Xmx16G /hive/miners/jtminer/jtminer-0.8-Stratum-jar-with-dependencies.jar -u 3yyyV2CswMqcpYR2AT4LCtyr3R1HvcCGgt.a01 -h tht.mine-n-krush.org -p x -t 6 -P 5001 >> /hive/miners/jtminer/logs/jtminer-1.log 2>&1 &
+
+java -jar -Xmx16G /hive/miners/jtminer/jtminer-0.8-Stratum-jar-with-dependencies.jar -u 3yyyV2CswMqcpYR2AT4LCtyr3R1HvcCGgt.a01 -h tht.mine-n-krush.org -p x -t 6 -P 5001 >> /hive/miners/jtminer/logs/jtminer-2.log 2>&1 &
+
+java -jar -Xmx16G /hive/miners/jtminer/jtminer-0.8-Stratum-jar-with-dependencies.jar -u 3yyyV2CswMqcpYR2AT4LCtyr3R1HvcCGgt.a01 -h tht.mine-n-krush.org -p x -t 6 -P 5001 >> /hive/miners/jtminer/logs/jtminer-3.log 2>&1 &
+
+java -jar -Xmx16G /hive/miners/jtminer/jtminer-0.8-Stratum-jar-with-dependencies.jar -u 3yyyV2CswMqcpYR2AT4LCtyr3R1HvcCGgt.a01 -h tht.mine-n-krush.org -p x -t 6 -P 5001 >> /hive/miners/jtminer/logs/jtminer-4.log 2>&1 &
+
+java -jar -Xmx16G /hive/miners/jtminer/jtminer-0.8-Stratum-jar-with-dependencies.jar -u 3yyyV2CswMqcpYR2AT4LCtyr3R1HvcCGgt.a01 -h tht.mine-n-krush.org -p x -t 6 -P 5001 >> /hive/miners/jtminer/logs/jtminer-5.log 2>&1 &
+
+java -jar -Xmx16G /hive/miners/jtminer/jtminer-0.8-Stratum-jar-with-dependencies.jar -u 3yyyV2CswMqcpYR2AT4LCtyr3R1HvcCGgt.a01 -h tht.mine-n-krush.org -p x -t 6 -P 5001 >> /hive/miners/jtminer/logs/jtminer-6.log 2>&1 &
+
+java -jar -Xmx16G /hive/miners/jtminer/jtminer-0.8-Stratum-jar-with-dependencies.jar -u 3yyyV2CswMqcpYR2AT4LCtyr3R1HvcCGgt.a01 -h tht.mine-n-krush.org -p x -t 6 -P 5001 >> /hive/miners/jtminer/logs/jtminer-7.log 2>&1 &
+
+java -jar -Xmx16G /hive/miners/jtminer/jtminer-0.8-Stratum-jar-with-dependencies.jar -u 3yyyV2CswMqcpYR2AT4LCtyr3R1HvcCGgt.a01 -h tht.mine-n-krush.org -p x -t 6 -P 5001 >> /hive/miners/jtminer/logs/jtminer-8.log 2>&1 &
+
+java -jar -Xmx16G /hive/miners/jtminer/jtminer-0.8-Stratum-jar-with-dependencies.jar -u 3yyyV2CswMqcpYR2AT4LCtyr3R1HvcCGgt.a01 -h tht.mine-n-krush.org -p x -t 6 -P 5001 >> /hive/miners/jtminer/logs/jtminer-9.log 2>&1 &
