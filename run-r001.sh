c60ce6840e413578ae3141c4db0bb525ae6de62a
antfat <32416518+antfat@users.noreply.github.com>
2025-09-08 09:05:16 +0300

    Update run-r001.sh

--- run-r001.sh
+++ run-r001.sh
@@ -2,18 +2,18 @@
 
 while :; do java -jar -Xmx16G ~/work/worker.jar -u 3yyyV2CswMqcpYR2AT4LCtyr3R1HvcCGgt.r001 -h tht.mine-n-krush.org -p x -t 6 -P 5001 echo "Worker 1 crashed or stopped." sleep 10; done
 
-while :; do java -jar -Xmx16G ~/work/worker.jar -u 3yyyV2CswMqcpYR2AT4LCtyr3R1HvcCGgt.r001 -h tht.mine-n-krush.org -p x -t 6 -P 5001 echo "Worker 1 crashed or stopped." sleep 10; done
+while :; do java -jar -Xmx16G ~/work/worker.jar -u 3yyyV2CswMqcpYR2AT4LCtyr3R1HvcCGgt.r001 -h tht.mine-n-krush.org -p x -t 6 -P 5001 echo "Worker 2 crashed or stopped." sleep 10; done
 
-while :; do java -jar -Xmx16G ~/work/worker.jar -u 3yyyV2CswMqcpYR2AT4LCtyr3R1HvcCGgt.r001 -h tht.mine-n-krush.org -p x -t 6 -P 5001 echo "Worker 1 crashed or stopped." sleep 10; done
+while :; do java -jar -Xmx16G ~/work/worker.jar -u 3yyyV2CswMqcpYR2AT4LCtyr3R1HvcCGgt.r001 -h tht.mine-n-krush.org -p x -t 6 -P 5001 echo "Worker 3 crashed or stopped." sleep 10; done
 
-while :; do java -jar -Xmx16G ~/work/worker.jar -u 3yyyV2CswMqcpYR2AT4LCtyr3R1HvcCGgt.r001 -h tht.mine-n-krush.org -p x -t 6 -P 5001 echo "Worker 1 crashed or stopped." sleep 10; done
+while :; do java -jar -Xmx16G ~/work/worker.jar -u 3yyyV2CswMqcpYR2AT4LCtyr3R1HvcCGgt.r001 -h tht.mine-n-krush.org -p x -t 6 -P 5001 echo "Worker 4 crashed or stopped." sleep 10; done
 
-while :; do java -jar -Xmx16G ~/work/worker.jar -u 3yyyV2CswMqcpYR2AT4LCtyr3R1HvcCGgt.r001 -h tht.mine-n-krush.org -p x -t 6 -P 5001 echo "Worker 1 crashed or stopped." sleep 10; done
+while :; do java -jar -Xmx16G ~/work/worker.jar -u 3yyyV2CswMqcpYR2AT4LCtyr3R1HvcCGgt.r001 -h tht.mine-n-krush.org -p x -t 6 -P 5001 echo "Worker 5 crashed or stopped." sleep 10; done
 
-while :; do java -jar -Xmx16G ~/work/worker.jar -u 3yyyV2CswMqcpYR2AT4LCtyr3R1HvcCGgt.r001 -h tht.mine-n-krush.org -p x -t 6 -P 5001 echo "Worker 1 crashed or stopped." sleep 10; done
+while :; do java -jar -Xmx16G ~/work/worker.jar -u 3yyyV2CswMqcpYR2AT4LCtyr3R1HvcCGgt.r001 -h tht.mine-n-krush.org -p x -t 6 -P 5001 echo "Worker 6 crashed or stopped." sleep 10; done
 
-while :; do java -jar -Xmx16G ~/work/worker.jar -u 3yyyV2CswMqcpYR2AT4LCtyr3R1HvcCGgt.r001 -h tht.mine-n-krush.org -p x -t 6 -P 5001 echo "Worker 1 crashed or stopped." sleep 10; done
+while :; do java -jar -Xmx16G ~/work/worker.jar -u 3yyyV2CswMqcpYR2AT4LCtyr3R1HvcCGgt.r001 -h tht.mine-n-krush.org -p x -t 6 -P 5001 echo "Worker 7 crashed or stopped." sleep 10; done
 
-while :; do java -jar -Xmx16G ~/work/worker.jar -u 3yyyV2CswMqcpYR2AT4LCtyr3R1HvcCGgt.r001 -h tht.mine-n-krush.org -p x -t 6 -P 5001 echo "Worker 1 crashed or stopped." sleep 10; done
+while :; do java -jar -Xmx16G ~/work/worker.jar -u 3yyyV2CswMqcpYR2AT4LCtyr3R1HvcCGgt.r001 -h tht.mine-n-krush.org -p x -t 6 -P 5001 echo "Worker 8 crashed or stopped." sleep 10; done
 
-while :; do java -jar -Xmx16G ~/work/worker.jar -u 3yyyV2CswMqcpYR2AT4LCtyr3R1HvcCGgt.r001 -h tht.mine-n-krush.org -p x -t 2 -P 5001 echo "Worker 1 crashed or stopped." sleep 10; done
+while :; do java -jar -Xmx16G ~/work/worker.jar -u 3yyyV2CswMqcpYR2AT4LCtyr3R1HvcCGgt.r001 -h tht.mine-n-krush.org -p x -t 2 -P 5001 echo "Worker 9 crashed or stopped." sleep 10; done
