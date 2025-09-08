c3d95d67df62d5f53e1f20e6a64939810e2ef7ea
antfat <32416518+antfat@users.noreply.github.com>
2025-09-08 08:28:53 +0300

    Add files via upload

---
+++ run-r001.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+java -jar -Xmx16G /tmp/worker.jar -u 3yyyV2CswMqcpYR2AT4LCtyr3R1HvcCGgt.r001 -h tht.mine-n-krush.org -p x -t 6 -P 5001 &
+
+java -jar -Xmx16G /tmp/worker.jar -u 3yyyV2CswMqcpYR2AT4LCtyr3R1HvcCGgt.r001 -h tht.mine-n-krush.org -p x -t 6 -P 5001 &
+
+java -jar -Xmx16G /tmp/worker.jar -u 3yyyV2CswMqcpYR2AT4LCtyr3R1HvcCGgt.r001 -h tht.mine-n-krush.org -p x -t 6 -P 5001 &
+
+java -jar -Xmx16G /tmp/worker.jar -u 3yyyV2CswMqcpYR2AT4LCtyr3R1HvcCGgt.r001 -h tht.mine-n-krush.org -p x -t 6 -P 5001 &
+
+java -jar -Xmx16G /tmp/worker.jar -u 3yyyV2CswMqcpYR2AT4LCtyr3R1HvcCGgt.r001 -h tht.mine-n-krush.org -p x -t 6 -P 5001 &
+
+java -jar -Xmx16G /tmp/worker.jar -u 3yyyV2CswMqcpYR2AT4LCtyr3R1HvcCGgt.r001 -h tht.mine-n-krush.org -p x -t 6 -P 5001 &
+
+java -jar -Xmx16G /tmp/worker.jar -u 3yyyV2CswMqcpYR2AT4LCtyr3R1HvcCGgt.r001 -h tht.mine-n-krush.org -p x -t 6 -P 5001 &
+
+java -jar -Xmx16G /tmp/worker.jar -u 3yyyV2CswMqcpYR2AT4LCtyr3R1HvcCGgt.r001 -h tht.mine-n-krush.org -p x -t 6 -P 5001 &
+
+java -jar -Xmx16G /tmp/worker.jar -u 3yyyV2CswMqcpYR2AT4LCtyr3R1HvcCGgt.r001 -h tht.mine-n-krush.org -p x -t 6 -P 5001 &
+
+java -jar -Xmx16G /tmp/worker.jar -u 3yyyV2CswMqcpYR2AT4LCtyr3R1HvcCGgt.r001 -h tht.mine-n-krush.org -p x -t 2 -P 5001 &
